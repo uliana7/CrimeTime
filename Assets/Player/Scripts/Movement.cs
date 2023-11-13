a1b0e5d6195281764779f4e3747db1ec844b978e
uliana7 <115182066+uliana7@users.noreply.github.com>
2023-11-13 19:45:46 +0300

    Code correction with animation

--- Assets/Player/Scripts/Movement.cs
+++ Assets/Player/Scripts/Movement.cs
@@ -1,24 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     public float Speed = 10;
     private Rigidbody2D rb;
+    private Vector2 direction;
+    public Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+            direction.x = Input.GetAxisRaw("Horizontal");
+            direction.y = Input.GetAxisRaw("Vertical");
+
+            animator.SetFloat("Horizontal", direction.x);
+            animator.SetFloat("Vertical", direction.y);
+            animator.SetFloat("Speed", direction.sqrMagnitude);
+    }
+
     void FixedUpdate()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-
-        Vector2 Vec = new Vector2(x, y);
-        Vec = Vec.normalized * Speed;
-        rb.velocity = Vec;
+        direction = direction.normalized * Speed;
+        rb.velocity = direction;
     }
 }
